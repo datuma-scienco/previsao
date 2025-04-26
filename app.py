49ecf96a67fa99ed00315b35ed88a39f1ae0341a
datuma-scienco <datuma.scienco@gmail.com>
2025-04-26 18:30:33 +0000

    Atualização automação

--- app.py
+++ app.py
@@ -6,57 +6,92 @@ import pandas as pd
 import plotnine as p9
 import mizani as mi
 from functools import partial
+from faicons import icon_svg
 
 # Funções
 def preparar_dados(parquet, y):
     df_tmp = pd.read_parquet(parquet).reset_index(names = "data")
-    periodo_prev = df_tmp.filter(["data", "Previsão"]).dropna().data
-    df_tmp.iloc[periodo_prev.index.min() - 1] = df_tmp.iloc[periodo_prev.index.min() - 1].ffill()
-    return df_tmp.melt(id_vars = ["data", "Intervalo Inferior", "Intervalo Superior"])
+    ult = df_tmp.query("Tipo == @y").query("data == data.max()")
+    df_tmp = pd.concat([
+        df_tmp, 
+        pd.DataFrame(
+            {
+                "data": ult.data.repeat(2), 
+                "Valor": ult.Valor.repeat(2), 
+                "Tipo": df_tmp.query("Tipo not in [@y, 'IA']").Tipo.unique().tolist(), 
+                "Intervalo Inferior": ult.Valor.repeat(2), 
+                "Intervalo Superior": ult.Valor.repeat(2)
+            }
+        )
+        ])
+    return df_tmp
 
 def gerar_grafico(df, y, n, unidade, linha_zero = True):
     dt = input.periodo()
-    df_tmp = (
-        df
-        .drop(
-            labels = ["Intervalo Inferior", "Intervalo Superior"],
-            axis = "columns"
-            )
-        .query("variable == @y and data >= @dt")
-        .dropna()
-        )
-    df_prev = (
-        df
-        .query("variable == 'Previsão'")
-        .dropna()
-        .head(n) # workaround para problema de groups do plotnine
-        )
+    mod = list(input.modelo())
+    mod.insert(0, y)
+    df_tmp = df.assign(
+        Tipo = lambda x: pd.Categorical(x.Tipo, mod)
+        ).query("data >= @dt and Tipo in @mod")
+
     def plotar_zero():
         if linha_zero:
             return p9.geom_hline(yintercept = 0, linetype = "dashed")
         else:
             return None
+
+    def plotar_ic():
+        if input.ic():
+            return  p9.geom_ribbon(
+                data = df_tmp,
+                mapping = p9.aes(
+                    ymin = "Intervalo Inferior",
+                    ymax = "Intervalo Superior",
+                    fill = "Tipo"
+                    ),
+                alpha = 0.25,
+                color = "none",
+                show_legend = False
+            ) 
+        else:
+            return None
+
     plt = (
         p9.ggplot(df_tmp) +
-        p9.aes(x = "data", y = "value", color = "variable") +
+        p9.aes(x = "data", y = "Valor", color = "Tipo") +
         plotar_zero() +
-        p9.geom_ribbon(
-            data = df_prev,
-            mapping = p9.aes(ymin = "Intervalo Inferior", ymax = "Intervalo Superior"),
-            alpha = 0.25 if input.ic() else 0, 
-            fill = "blue", 
-            color = "none"
-            ) +
+        plotar_ic() +
         p9.geom_line() +
-        p9.geom_line(data = df_prev, group = 1) +
         p9.scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
         p9.scale_y_continuous(breaks = mi.breaks.breaks_extended(6)) +
-        p9.scale_color_manual(values = {y: "black", "Previsão": "blue"}) +
+        p9.scale_color_manual(
+            values = {
+                y: "black", 
+                "IA": "green",
+                "Ridge": "blue",
+                "Bayesian Ridge": "orange",
+                "Huber": "red",
+                "Ensemble": "brown"
+                },
+            drop = True,
+            breaks = df_tmp.Tipo.unique().tolist()
+            ) +
+        p9.scale_fill_manual(
+            values = {
+                "IA": "green",
+                "Ridge": "blue",
+                "Bayesian Ridge": "orange",
+                "Huber": "red",
+                "Ensemble": "brown"
+                },
+            drop = True
+            ) +
         p9.labs(color = "", x = "", y = unidade) +
         p9.theme(
             panel_grid_minor = p9.element_blank(),
             legend_position = "bottom"
             )
+
     )
     return plt
 
@@ -66,80 +101,175 @@ df_cambio = preparar_dados("previsao/cambio.parquet", "Câmbio")
 df_pib = preparar_dados("previsao/pib.parquet", "PIB")
 df_selic = preparar_dados("previsao/selic.parquet", "Selic")
 
+lista_modelos = list(set(
+    df_ipca.query("Tipo != 'IPCA'").Tipo.unique().tolist() +
+    df_cambio.query("Tipo != 'Câmbio'").Tipo.unique().tolist() +
+    df_pib.query("Tipo != 'PIB'").Tipo.unique().tolist() +
+    df_selic.query("Tipo != 'Selic'").Tipo.unique().tolist()
+))
+
+
 # Layout
 ui.page_opts(
     title = ui.span(
         ui.img(
-            src = "https://aluno.analisemacro.com.br/download/59787/?tmstv=1712933415",
+            src = "https://camo.githubusercontent.com/024e09392fb5f5bc918d315821a2117b44194d931a4a0f9bb493598c138d59c5/68747470733a2f2f63646e2e69636f6e73636f75742e636f6d2f69636f6e2f7072656d69756d2f706e672d3235362d7468756d622f646174612d616e616c797369732d323135373739352d313831333431322e706e67",
             height = 30,
             style = "padding-right:10px;"
             )
     ),
     window_title = "Painel de Previsões",
     page_fn = partial(page_navbar, fillable = True),
-    theme = theme.minty
+    theme = theme.lumen,
 )
 
-with ui.nav_panel("Painel de Previsões"):  
+with ui.nav_panel("Painel de Previsões Macroeconômicas"):  
     with ui.layout_sidebar():
-        
+
         # Inputs
         with ui.sidebar(width = 225):
-            ui.input_slider(
-                id = "h",
-                label = "Horizonte de previsão:",
-                min = 1,
-                max = 12,
-                value = 12,
-                step = 1, 
+
+            # Informação
+            ui.markdown(
+                (
+                    "Acompanhe as previsões automatizadas dos principais " +
+                    "indicadores macroeconômicos do Brasil e simule cenários" +
+                    " alternativos em um mesmo dashboard."
+                )
+            )
+
+            ui.input_selectize(
+                id = "modelo",
+                label = ui.strong("Selecionar modelos:"),
+                choices = lista_modelos,
+                selected = lista_modelos,
+                multiple = True,
                 width = "100%",
-                post = "m"
+                options = {"plugins": ["clear_button"]}
                 )
             ui.input_date(
                 id = "periodo",
-                label = "Início do gráfico:",
+                label = ui.strong("Início do gráfico:"),
                 value = pd.to_datetime("today") - pd.offsets.YearBegin(7),
                 min = "2004-01-01",
                 max = df_selic.data.max(),
                 language = "pt-BR",
-                width = "100%"
+                width = "100%",
+                format = "mm/yyyy",
+                startview = "year"
                 )
             ui.input_checkbox(
                 id = "ic",
-                label = "Intervalo de confiança",
+                label = ui.strong("Intervalo de confiança"),
                 value = True
             )
-        
+
+            # Informação
+            ui.markdown(
+                "<b>Elaboração:</b><br>Evandro Avellar"
+            )
+
         # Outputs
         with ui.layout_column_wrap():
-            
-            with ui.card():
-                ui.card_header(ui.strong("Inflação (IPCA)"))
-                @render.plot
-                def ipca():
-                    plt = gerar_grafico(df_ipca, "IPCA", input.h() + 1, "Var. %")
-                    return plt
 
-            with ui.card():
-                ui.card_header(ui.strong("Taxa de Câmbio (BRL/USD)"))
-                @render.plot
-                def cambio():
-                    plt = gerar_grafico(df_cambio, "Câmbio", input.h() + 1, "R\\$/US\\$", False)
-                    #plt = gerar_grafico(df_cambio, "Câmbio", input.h() + 1, "R", False)
-                    return plt
+            with ui.navset_card_underline(title = ui.strong("Inflação (IPCA)")):
+                with ui.nav_panel("", icon = icon_svg("chart-line")):
+                    @render.plot
+                    def ipca1():
+                        plt = gerar_grafico(df_ipca, "IPCA", input.modelo(), "Var. %")
+                        return plt
+
+                with ui.nav_panel(" ", icon = icon_svg("table")):
+                    @render.data_frame
+                    def ipca2():
+                        df_tmp = (
+                            df_ipca
+                            .query("Tipo != 'IPCA'")
+                            .rename(
+                                columns = {
+                                    "Valor": "Previsão", 
+                                    "Tipo": "Modelo", 
+                                    "data": "Data"}
+                                )
+                            .assign(Data = lambda x: x.Data.dt.strftime("%m/%Y"))
+                            .round(2)
+                            .head(-2)
+                        )
+                        return render.DataGrid(df_tmp, summary = False)
+
+            with ui.navset_card_underline(title = ui.strong("Taxa de Câmbio (BRL/USD)")):
+                with ui.nav_panel("", icon = icon_svg("chart-line")):
+                    @render.plot
+                    def cambio1():
+                        plt = gerar_grafico(df_cambio, "Câmbio", input.modelo(), "R\\$/US\\$", False)
+                        return plt
+
+                with ui.nav_panel(" ", icon = icon_svg("table")):
+                    @render.data_frame
+                    def cambio2():
+                        df_tmp = (
+                            df_cambio
+                            .query("Tipo != 'Câmbio'")
+                            .rename(
+                                columns = {
+                                    "Valor": "Previsão", 
+                                    "Tipo": "Modelo", 
+                                    "data": "Data"}
+                                )
+                            .assign(Data = lambda x: x.Data.dt.strftime("%m/%Y"))
+                            .round(2)
+                            .head(-2)
+                        )
+                        return render.DataGrid(df_tmp, summary = False)
 
         with ui.layout_column_wrap():
-            
-            with ui.card():
-                ui.card_header(ui.strong("Atividade Econômica (PIB)"))
-                @render.plot
-                def pib():
-                    plt = gerar_grafico(df_pib, "PIB", (input.h()//3) + 1, "Var. % anual")
-                    return plt
 
-            with ui.card():
-                ui.card_header(ui.strong("Taxa de Juros (SELIC)"))
-                @render.plot
-                def selic():
-                    plt = gerar_grafico(df_selic, "Selic", input.h() + 1, "% a.a.", False)
-                    return plt
+            with ui.navset_card_underline(title = ui.strong("Atividade Econômica (PIB)")):
+                with ui.nav_panel("", icon = icon_svg("chart-line")):
+                    @render.plot
+                    def pib1():
+                        plt = gerar_grafico(df_pib, "PIB", input.modelo(), "Var. % anual")
+                        return plt
+
+                with ui.nav_panel(" ", icon = icon_svg("table")):
+                    @render.data_frame
+                    def pib2():
+                        df_tmp = (
+                            df_pib
+                            .query("Tipo != 'PIB'")
+                            .rename(
+                                columns = {
+                                    "Valor": "Previsão", 
+                                    "Tipo": "Modelo", 
+                                    "data": "Data"}
+                                )
+                            .assign(Data = lambda x: x.Data.dt.to_period(freq = "Q").dt.strftime("T%q/%Y"))
+                            .round(2)
+                            .head(-2)
+                        )
+                        return render.DataGrid(df_tmp, summary = False)
+
+            with ui.navset_card_underline(title = ui.strong("Taxa de Juros (SELIC)")):
+                with ui.nav_panel("", icon = icon_svg("chart-line")):
+                    @render.plot
+                    def selic1():
+                        plt = gerar_grafico(df_selic, "Selic", input.modelo, "% a.a.", False)
+                        return plt
+
+                with ui.nav_panel(" ", icon = icon_svg("table")):
+                    @render.data_frame
+                    def selic2():
+                        df_tmp = (
+                            df_selic
+                            .query("Tipo != 'Selic'")
+                            .rename(
+                                columns = {
+                                    "Valor": "Previsão", 
+                                    "Tipo": "Modelo", 
+                                    "data": "Data"}
+                                )
+                            .assign(Data = lambda x: x.Data.dt.strftime("%m/%Y"))
+                            .round(2)
+                            .head(-2)
+                        )
+                        return render.DataGrid(df_tmp, summary = False)
